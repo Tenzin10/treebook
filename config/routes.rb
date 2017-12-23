2bbe70f09cc26f3e4b55c3a4397ac464936ac6b2
Tenzin10 <tenzin.dorjee.1534@gmail.com>
2017-12-23 23:32:17 +0600

    added custom routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users, :controllers => { :registrations => 'registrations'}
+  devise_scope :user do
+  get 'register', to: 'devise/registrations#new', as: :register
+  get 'login', to: 'devise/sessions#new', as: :login
+end
   resources :statuses
     root to: "statuses#index"
 
