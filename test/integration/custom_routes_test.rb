95c521f909a59e66f406a773a3e6aa0cd9825caf
Tenzin10 <tenzin.dorjee.1534@gmail.com>
2017-12-24 11:25:49 +0600

    added test custom routes

---
+++ test/integration/custom_routes_test.rb
@@ -0,0 +1,18 @@
+require 'test_helper'
+
+class CustomRoutesTest < ActionDispatch::IntegrationTest
+  # test "the truth" do
+  #   assert true
+  # end
+
+  test '/login route opens login page' do
+  		get '/login'
+  		assert_response :success
+  end
+
+  test '/register route opens register page' do
+  		get '/register'
+  		assert_response :success
+
+  end
+end
